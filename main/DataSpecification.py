15e897478c3f5d243949856830e2948ad211f612
Weiyu Liu <wliu88@gatech.edu>
2019-08-25 17:29:32 -0400

    Started coding first model

--- main/DataSpecification.py
+++ main/DataSpecification.py
@@ -7,3 +7,7 @@ STATES = {"cup": ["hot", "cold", "empty"],
           "spatula": ["has stuff", "empty"],
           "bottle": ["lid on", "lid off"],
           "pan": ["hot", "empty"]}
+
+MATERIALS = {"plastic", "metal", "ceramic", "glass", "paper", "stone", "wood"}
+
+AFFORDANCES = {"opening", "body", "handle", "grasp", "w_grasp", "contain", "support", "pound", "display"}
